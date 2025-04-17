5a5b24883249500e8df5886188ffaad4e42f1eed
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-04-17 11:16:59 +0200

    feat: plotting script for 2d motion test case

---
+++ src/motion/plot_results.py
@@ -0,0 +1,25 @@
+"""
+Copyright (c) 2025 MPI-M, Clara Bayley
+
+
+----- ValidatingCLEO -----
+File: plot_results.py
+Project: motion
+Created Date: Thursday 17th April 2025
+Author: Clara Bayley (CB)
+Additional Contributors:
+-----
+Last Modified: Thursday 17th April 2025
+Modified By: CB
+-----
+License: BSD 3-Clause "New" or "Revised" License
+https://opensource.org/licenses/BSD-3-Clause
+-----
+File Description:
+Source files to plot SD (as tracers) motion in flow similar to figure 1 of
+Arabas et a. 2015 2-D model
+"""
+
+
+def plot_results(path2pySD, grid_filename, datasets, setups):
+    print("WORK IN PROGRESS")
