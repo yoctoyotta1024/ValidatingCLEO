89f86eb6b16d7d7333c57f658095ff4848f2d4db
clara.bayley <clara.bayley@mpimet.mpg.de>
2025-09-02 16:02:03 +0200

    feat: bump version control

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,7 +43,7 @@ project = "ValidatingCLEO"
 license = "BSD 3-Clause"
 copyright = "(2025) MPI-M, Clara Bayley"
 author = "Clara Bayley & Other Developers"
-release = "0.0.0"
+release = "0.6.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ValidatingCLEO",
-    version="0.0.0",
+    version="0.6.0",
     packages=find_packages(),
     install_requires=[
         "pytest",
